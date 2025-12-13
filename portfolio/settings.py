59c6bfce408bffd389445be8df91a15dab52bc76
patilhetal <patilhetal60@gmail.com>
2025-12-13 22:37:33 +0530

    added staticfiles

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -21,7 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+
+
+SECRET_KEY = os.environ.get("SECRET_KEY", "django-insecure-temp-key-for-local")
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
@@ -123,17 +126,23 @@ USE_I18N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = "static/"
 
-# addded manually
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+STATIC_URL = '/static/'
+
+# Where collectstatic will collect files
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# Optional (your existing static folder)
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# WhiteNoise compression
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
